64e9ce30728db27f194633199f1b52b80372bb02
pleveque <pierreleveque@gmail.com>
2022-07-03 11:00:23 +0200

    start add game

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import ChatIcon from './component/ChatIcon';
 import Game from './component/Game';
+import Header from './component/Header';
 import './main.scss'
 
 function App() {
+	const constraintsRef = useRef(null);
+
 	return (
 		<div className="App">
-			<Game />
+			<Header />
+			<div className="workZone flexContainer" ref={constraintsRef}>
+				<ChatIcon constraintsRef={constraintsRef}/>
+				<Game />
+			</div>
+			<Header />
 		</div>
 	);
 }
